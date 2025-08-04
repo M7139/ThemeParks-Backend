01a80724e11fc1b91691a8f29fe485dce6ec4a57
feras-3 <ferasdarwish02@gmail.com>
2025-08-04 15:05:15 +0300

    crud operations done

--- controllers/RideController.js
+++ controllers/RideController.js
@@ -1,3 +1,50 @@
 const { Ride } = require('../models')
 
-module.exports = {}
+const GetRides = async (req, res) => {
+  try {
+    const rides = await Ride.find({})
+    res.send(rides)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const CreateRide = async (req, res) => {
+  try {
+    const ride = await Ride.create({ ...req.body })
+    res.send(ride)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const ShowRide = async (req, res) => {
+  try {
+    const ride = await Ride.findById(req.params.ride_id)
+    res.send(ride)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const UpdatePost = async (req, res) => {
+  try {
+    const ride = await Ride.findByIdAndUpdate(req.params.ride_id, req.body, {
+      new: true
+    })
+    res.send(ride)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const DeleteRide = async (req, res) => {
+  try {
+    await Ride.deleteOne({ _id: req.params.ride_id })
+    res.send({ msg: 'ride deleted', payload: req.params.ride_id, status: 'ok' })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { GetRides, CreateRide, ShowRide, UpdatePost, DeleteRide }
--- routes/RideRouter.js
+++ routes/RideRouter.js
@@ -1,4 +1,11 @@
 const router = require('express').Router()
 const controller = require('../controllers/RideController')
 
+router.get('/', controller.GetRides)
+router.post('/', controller.CreateRide)
+
+router.get('/:ride_id', controller.ShowRide)
+router.put('/:ride_id', controller.UpdatePost)
+router.delete('/:ride_id', controller.DeleteRide)
+
 module.exports = router
