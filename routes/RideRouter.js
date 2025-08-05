e4dd93c23a01d34b32f1d7f65a734d1a1dc5c48e
feras-3 <ferasdarwish02@gmail.com>
2025-08-05 21:53:03 +0300

    added SearchRide controller

--- routes/RideRouter.js
+++ routes/RideRouter.js
@@ -4,10 +4,10 @@ const controller = require('../controllers/RideController')
 router.get('/', controller.GetRides)
 router.post('/', controller.CreateRide)
 
+router.get('/search/:search_id', controller.SearchRide)
+
 router.get('/:ride_id', controller.GetRide)
 router.put('/:ride_id', controller.UpdateRide)
 router.delete('/:ride_id', controller.DeleteRide)
-
-
 
 module.exports = router
