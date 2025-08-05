75816f290f037cf6c899842cf21d120b17453d78
feras-3 <ferasdarwish02@gmail.com>
2025-08-05 09:26:53 +0300

    fix conflicts pushed

--- routes/RideRouter.js
+++ routes/RideRouter.js
@@ -2,16 +2,14 @@ const router = require('express').Router()
 const controller = require('../controllers/RideController')
 
 router.get('/', controller.GetRides)
-<<<<<<< HEAD
 router.get('/:ride_id', controller.GetRide)
 router.post('/', controller.CreateRide)
 router.put('/:ride_id', controller.UpdateRide)
-=======
-router.post('/', controller.CreateRide)
 
-router.get('/:ride_id', controller.ShowRide)
-router.put('/:ride_id', controller.UpdatePost)
->>>>>>> ada96262de9cabb77c7140439a619972ac426145
-router.delete('/:ride_id', controller.DeleteRide)
+// router.post('/', controller.CreateRide)
+
+// router.get('/:ride_id', controller.ShowRide)
+// router.put('/:ride_id', controller.UpdatePost)
+// router.delete('/:ride_id', controller.DeleteRide)
 
 module.exports = router
